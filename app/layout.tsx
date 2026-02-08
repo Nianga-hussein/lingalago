321fd3b2f73ff3f8f105f4194b61d54a965b0403
Nianga-hussein <niangahussein93@gmail.com>
2026-02-08 18:13:26 +0100

    OOOY

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="fr" className={nunito.variable}>
+    <html lang="fr">
       <body className="font-sans antialiased">{children}</body>
     </html>
   );
